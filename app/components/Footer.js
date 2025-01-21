11804684efe134437a1f8d481b48e66cec630a12
rabiulislamrubel <robiul91912113@gmail.com>
2025-01-21 10:38:33 +0600

    year bug fix

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,10 +2,11 @@
 
 export default function Footer() {
     const date = new Date();
+    const year = date.getFullYear();
     
     return (
         <footer className="h-12 bg-[#95A5A7] 2xl:h-20 center text-shadow font-bold lg:justify-end md:px-16 xl:px-28 2xl:px-40 2xl:text-xl">
-            {date.getFullYear()} &copy; Created by Rabiul Islam.
+            {year} &copy; Created by Rabiul Islam.
         </footer>
     )
 }
